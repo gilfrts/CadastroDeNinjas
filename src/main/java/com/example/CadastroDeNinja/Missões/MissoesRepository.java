c722f57274d8cf1c3cc2488cadf95c4e8a2cd98a
Gilberto Freitas <gilbertogabrielslop@gmail.com>
2026-02-24 11:12:32 -0300

    feat: Adição do repository das missões

---
+++ src/main/java/com/example/CadastroDeNinja/Missões/MissoesRepository.java
@@ -0,0 +1,6 @@
+package com.example.CadastroDeNinja.Missões;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
+}
---
+++ src/main/java/com/example/CadastroDeNinja/Ninjas/NinjaRepository.java
@@ -0,0 +1,4 @@
+package com.example.CadastroDeNinja.Ninjas;
+
+public class NinjaRepository {
+}
